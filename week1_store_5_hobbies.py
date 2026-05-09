237f17f09a841f038feb96bce6fdf1a53cc632be
eadenr2026 <eadenr2026@gmail.com>
2026-05-09 14:20:50 -0700

    week1 hobbies and food program

---
+++ week1_store_5_hobbies.py
@@ -0,0 +1,27 @@
+# Write a program that stores 5 hobbies in a list and prints each one with 'Rowland likes:' in front
+
+hobbies = [
+    "Video Games",
+    "Coding",
+    "Music",
+    "Animation",
+    "Sports"
+]
+
+foods = [
+    "Pizza",
+    "Chicken",
+    "Pasta",
+    "Steak",
+    "Ice cream"
+]
+
+
+for hobby in hobbies:
+    print(f"Rowland likes: {hobby}")
+
+print()
+
+for food in foods:
+    print(f"Rowland likes: {food}")
+
