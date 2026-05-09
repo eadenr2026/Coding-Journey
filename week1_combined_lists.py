e490b20a1cc040ad337bee6e6f1d401efc4d844f
eadenr2026 <eadenr2026@gmail.com>
2026-05-09 14:43:55 -0700

    week1 combined list program

---
+++ week1_combined_lists.py
@@ -0,0 +1,22 @@
+# list of hobbies
+hobbies = [
+    "Video Games",
+    "Music",
+    "Coding",
+    "Sports",
+    "Learning"
+]
+# list of foods
+foods = [
+    "Pizza",
+    "Chicken",
+    "Steak",
+    "Pasta",
+    "Ice cream"
+]
+# combined list using + operator
+hobbies_and_foods = hobbies + foods
+
+# Loop through each item in list, and print string with list items
+for x in hobbies_and_foods:
+    print(f"Rowland likes: {x}")
